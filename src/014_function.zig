f094ac3301016b88410335ffb8271fe79c0b4d37
AZ08867 <2049482458@qq.com>
2025-12-14 20:49:47 +0800

    feat(core): 添加计算2的幂函数
    在main函数中调用了twoToThe函数，并打印了2的1次方到4次方的结果。
    在deepThought函数下方添加了twoToThe函数的定义，该函数使用std.math.pow计算2的幂。

--- src/014_function.zig
+++ src/014_function.zig
@@ -3,6 +3,9 @@ const std = @import("std");
 pub fn main() void {
     const answer: u8 = deepThought();
     std.debug.print("Answer to the Ultimate Question: {}\n", .{answer});
+
+    // 调用twoToThe函数
+    std.debug.print("Powers of two: {} {} {} {}\n", .{ twoToThe(1), twoToThe(2), twoToThe(3), twoToThe(4) });
 }
 
 // 定义 deepThought 函数
@@ -13,8 +16,19 @@ fn deepThought() u8 {
     return 42;
 }
 
-// 语法：
+// 语法 examples：
 //     fn foo(n: u8) u8 {
 //         return n + 1;
 //     }
 //
+
+//     fn myFunction(number: u8, is_lucky: bool) {
+//         ...
+//     }
+
+fn twoToThe(my_number: u32) u32 {
+    // std.math.pow(T,x,y)
+    // T: 返回的结果类型
+    // 计算 x^y
+    return std.math.pow(u32, 2, my_number);
+}
