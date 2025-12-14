7b8f391f5a4f28e2d7a5274ce457f7a022be2ecd
AZ08867 <2049482458@qq.com>
2025-12-14 20:23:04 +0800

    feat(core): 添加 deepThought 简单示例函数实现

---
+++ src/014_function.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+pub fn main() void {
+    const answer: u8 = deepThought();
+    std.debug.print("Answer to the Ultimate Question: {}\n", .{answer});
+}
+
+// 定义 deepThought 函数
+// 1. 不需要参数 ()
+// 2. 返回类型是 u8
+// 3. 这里的 42 刚好在 u8 (0-255) 的范围内
+fn deepThought() u8 {
+    return 42;
+}
